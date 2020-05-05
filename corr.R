2204b055fe6804f49d4ff5c8dcc239e052868730
callumjethomas <callumthomas@outlook.com>
2020-05-05 13:37:54 +0100

    Initial commit of script for Assignment 1, part 3.

---
+++ corr.R
@@ -0,0 +1,6 @@
+corr <- function(directory, threshold = 0){ 
+        # directory : string containing the path
+        # threshold : numeric, number of complete observations required to calculate correlation
+        
+        
+}
