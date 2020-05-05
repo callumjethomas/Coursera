3b0b85a1284c1498feed2c8e017fa96c02e9137f
callumjethomas <callumthomas@outlook.com>
2020-05-05 13:18:36 +0100

    Completed Assignment 1, part 2.
    Got rid of range and only iterated using 'id'. Used complete.cases to get observations, then nrows to count.

--- complete.R
+++ complete.R
@@ -2,19 +2,28 @@ complete <- function(directory, id = 1:332){
         # directory : string containing the path
         # id : integer vector containing monitor IDs
         
-        # DEBUG:
-        directory <- "C:/Users/callu/Desktop/specdata"
-        id <- 1:332
-        
+        # Create a blank data frame.
+        n_obs <- data.frame()
         #  Get files from directory.
         filelist <- dir(directory)
-        # Get range indicated by 'id'
-        range <- id[1]:id[length(id)]
-        # For each file in the directory up in the range indicated by 'id',
-        for(file in filelist[range]) {
+        # For each file in the directory, in the range indicated by 'id',
+        for(file in filelist[id]) {
                 # Create the path by pasting the directory and file name together.
                 path <- paste(directory, file, sep = "/")
                 # Read the data.
                 data <- read.csv(path, header = TRUE, sep = ",", 
                                  colClasses = c("Date", "numeric", "numeric", 
                                                 "numeric"))
+                # Get only complete cases from the data.
+                compl <- data[complete.cases(data), ]
+                # Count number of complete cases.
+                count <- nrow(compl)
+                # Create a row containing the id number and number of complete cases.
+                row <- cbind(data$ID[1], count)
+                # Add this row to the data frame.
+                n_obs <- rbind(n_obs, row)
+        }
+        # Rename data frame columns and print.
+        colnames(n_obs) <- c("id", "nobs")
+        n_obs
+}
