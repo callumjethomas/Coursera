3cc4755ca1cf24601b18a1797c02ef40228264d1
callumjethomas <callumthomas@outlook.com>
2020-05-05 13:40:19 +0100

    Added section to read the data from the directory in a loop.

--- corr.R
+++ corr.R
@@ -2,5 +2,14 @@ corr <- function(directory, threshold = 0){
         # directory : string containing the path
         # threshold : numeric, number of complete observations required to calculate correlation
         
-        
+        #  Get files from directory.
+        filelist <- dir(directory)
+        for(file in filelist) {
+                # Create the path by pasting the directory and file name together.
+                path <- paste(directory, file, sep = "/")
+                # Read the data.
+                data <- read.csv(path, header = TRUE, sep = ",", 
+                                 colClasses = c("Date", "numeric", "numeric", 
+                                                "numeric"))
+        }
 }
