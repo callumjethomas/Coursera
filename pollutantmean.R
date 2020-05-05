cf9e3e991c907c7fbf992e9e8fb7a52c8ed87512
callumjethomas <callumthomas@outlook.com>
2020-05-05 12:56:57 +0100

    Completed Assignment 1, part 1.
    Added final sum to calculate WEIGHTED mean.
    
    Changed iteration to iterate over range of id[subset] and fixed issue where nitrate was introducing NaN by stripping the meanlist of missing values before computing weighted mean.

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,23 +1,35 @@
 pollutantmean <- function(directory, pollutant, id = 1:332){
-        directory <- "C:/Users/callu/Desktop/specdata"
-        pollutant <- "sulfate"
-        id <- 1:10
-        i = id[1] - 1
-        meanlist <- data.frame()
-        filelist <- dir(directory)
-        for(file in filelist[1:length(id)]) {
-                i = i+1
-                path <- paste(directory, file, sep = "/")
-                data <- read.csv(path, header = TRUE, sep = ",", 
-                                 colClasses = c("Date", "numeric", "numeric", 
-                                                "numeric"))
-                id_mean <- mean(data[[pollutant]], na.rm = TRUE)
-                row <- cbind(i, id_mean)
-                meanlist <- rbind(meanlist, row)
-        }
-        mean(meanlist$id_mean)
-}
         # directory : string containing the path
         # pollutant : string naming the pollutant
         # id : integer vector containing monitor IDs
         
+        # Create blank mean list.
+        meanlist <- data.frame()
+        #  Get files from directory.
+        filelist <- dir(directory)
+        # Get range indicated by 'id'
+        range <- id[1]:id[length(id)]
+        # For each file in the directory up in the range indicated by 'id',
+        for(file in filelist[range]) {
+                # Create the path by pasting the directory and file name together.
+                path <- paste(directory, file, sep = "/")
+                # Read the data.
+                data <- read.csv(path, header = TRUE, sep = ",", 
+                                 colClasses = c("Date", "numeric", "numeric", 
+                                                "numeric"))
+                # Calculate the mean of non-missing values for the pollutant.
+                id_mean <- mean(data[[pollutant]], na.rm = TRUE)
+                # Calculate the number of non-missing values for the pollutant.
+                id_sum <- sum(!is.na(data[[pollutant]]), na.rm = TRUE)
+                # Multiply mean by number of observations (to calculate weighting).
+                weight <- id_mean * id_sum
+                # Bind all these columns together into one row.
+                row <- cbind(id_mean, id_sum, weight)
+                # Add this row to the mean list.
+                meanlist <- rbind(meanlist, row)
+                # Strip out missing values.
+                meanlist <- meanlist[complete.cases(meanlist),]
+        }
+        # Divide the sum of all the weights by the sum of all the observations to obtain the weighted mean.
+        sum(meanlist$weight)/sum(meanlist$id_sum)
+}
