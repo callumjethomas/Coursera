d5c4be29b744da629281c5c171e00996cc443f8b
callumjethomas <callumthomas@outlook.com>
2020-05-05 14:16:01 +0100

    Finished writing script for Assignment 1, part 3. Added call of function from part 2 (complete).

--- corr.R
+++ corr.R
@@ -2,14 +2,33 @@ corr <- function(directory, threshold = 0){
         # directory : string containing the path
         # threshold : numeric, number of complete observations required to calculate correlation
         
-        #  Get files from directory.
+        # Create empty vector
+        cor_vec <- c()
+        
+        # Get number of observations per monitor using "complete" function.
+        n_obs <- complete(directory)
+        
+        # Get list of monitors with observations over the threshold.
+        obs_thr <- n_obs[which(n_obs$nobs > threshold),]
+        mon_list <- obs_thr$id
+        
         filelist <- dir(directory)
-        for(file in filelist) {
+        # For each file in the directory included in the monitor list,
+        for(file in filelist[mon_list]) {
                 # Create the path by pasting the directory and file name together.
                 path <- paste(directory, file, sep = "/")
                 # Read the data.
                 data <- read.csv(path, header = TRUE, sep = ",", 
                                  colClasses = c("Date", "numeric", "numeric", 
                                                 "numeric"))
+                # Strip missing values.
+                data <- data[complete.cases(data), ]
+                # Get correlation between pollutants.
+                correlat <- cor(data$sulfate, data$nitrate)
+                # Add correlation to vector.
+                cor_vec <- c(cor_vec, correlat)
         }
+        # Output vector.
+        cor_vec
+
 }
