80e622368e3f004b9eaa64fcd08673b4dbbc2c2c
callumjethomas <callumthomas@outlook.com>
2020-05-08 14:07:43 +0100

    Removed debugging code.

--- rankall.R
+++ rankall.R
@@ -1,9 +1,5 @@
 rankall <- function(outcome, num){
-        
-        # DEBUG
-        outcome = "heart attack"
-        num = "best"
-        
+
         # Read outcome data
         data <- read.csv("outcome-of-care-measures.csv", 
                          colClasses = "character")
