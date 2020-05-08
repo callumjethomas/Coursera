5c87075ecb253a2990e3a8009d157e131dfa23fe
callumjethomas <callumthomas@outlook.com>
2020-05-08 13:11:56 +0100

    Fixed formatting of if / else steps to be compliant with R standard.

--- best.R
+++ best.R
@@ -7,21 +7,24 @@ best <- function(state, outcome) {
         subdata <- subset(data, data$State == state)
         
         # Throw error if subsetting by state returns no rows (invalid)
-        if(nrow(subdata) == 0)
-                stop("invalid state") else
-        
-        # Subset by outcome, select hospital name and relevant 30-day death column
-        if(outcome == "heart attack")
-                subdata <- subset(subdata, select = c(2, 11)) else
-                        
-        if(outcome == "heart failure")
-                subdata <- subset(subdata, select = c(2, 17)) else
-                        
-        if(outcome == "pneumonia")
-                subdata <- subset(subdata, select = c(2, 23)) else
-                        
+        if (nrow(subdata) == 0) {
+                stop("invalid state") 
+                
+                # Subset by outcome, select hospital name and relevant 30-day death column
+        } else if (outcome == "heart attack") {
+                subdata <- subset(subdata, select = c(2, 11))
+                
+        } else if (outcome == "heart failure") {
+                subdata <- subset(subdata, select = c(2, 17)) 
+                
+        } else if (outcome == "pneumonia") {
+                subdata <- subset(subdata, select = c(2, 23)) 
+                
                 # Throw error if neither of the three possible outcomes are input
+        } else {
                 stop("invalid outcome")
+                
+        }
         
         # Make the 30-day death data numeric (suppress NA warning)
         suppressWarnings(subdata[,2] <- as.numeric(subdata[,2]))
@@ -30,7 +33,7 @@ best <- function(state, outcome) {
         clean <- subdata[complete.cases(subdata), ]
         
         # Sort by 30-day deaths (low to high) and then alphabetically by hospital name
-        sorted <- clean[order(clean[,2], clean[,1]),]
+        sorted <- clean[order(clean[, 2], clean[, 1]), ]
    
         # Return hospital name in state with lowest 30-day death rate for the outcome
         s <- sorted[1,1]
